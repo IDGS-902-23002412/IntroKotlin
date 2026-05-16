4fb03b6fefe36bb74e857482dabbe59e1d7bcdb0
IDGS-802-23002412 <rdannabuendia@gmail.com>
2026-05-16 09:09:11 -0600

    Clases 16/05/26

---
+++ ejemplo9/temasClass.kt
@@ -0,0 +1,43 @@
+/*
+La estructura básica en Kotlin de una clase es:
+
+class [nombre de la clase]{
+    [propiedades de la clase]
+    [métodos o funciones de la clase]
+}
+ */
+
+class Persona {
+    var nombre: String = ""
+    var edad: Int = 0 
+
+    fun inicializar(nombre: String, edad: Int){
+        this.nombre = nombre
+        this.edad = edad
+    }
+
+    fun imprimir(){
+        println("Nombre: $nombre y tiene una edad de $edad")
+    }
+
+    fun esMayorEdad(){
+        if(edad >= 18)
+            println("Es mayor de edad $nombre")
+        else
+            println("No es mayor de edad $nombre")
+    }
+}
+
+fun main (parametro: Array<String>){
+    val persona1: Persona
+    persona1 = Persona()
+    persona1.inicializar("Juan", 12)
+    persona1.imprimir()
+    persona.esMayorEdad()
+    
+    val persona2: Persona
+    persona2 = Persona()
+    persona2.inicializar("Ana", 50)
+    persona2.imprimir()
+    persona2.esMayorEdad()
+}
